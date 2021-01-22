3adf4aa351c78bdcc6243555cf9644a7b310bc50
panovyk <lromikl@ukr.net>
2021-01-22 21:29:36 +0200

    developing framework -added new methods for html

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,16 +1,28 @@
+import {$} from '@core/dom'
+
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+    this.$el = $(selector)
     this.components = options.components || []
   }
 
   getRoot() {
-    const $root = document.createElement('div')
-    $root.textContent = 'text'
+    const $root = $.create('div', 'excel')
+    // mb future err
+    this.components.forEach( (Component) => {
+      const $el = $.create('div', Component.className)
+      const component = new Component($el)
+      $el.html(component.toHTML())
+      $root.append($el)
+      // eslint-disable-next-line max-len
+      // $root.insertAdjacentHTML('beforeend', component.toHTML()) - old method without style
+    })
     return $root
   }
 
   render() {
+    const node = document.createElement('h1')
+    node.textContent = 'test321'
     this.$el.append(this.getRoot())
   }
 }
