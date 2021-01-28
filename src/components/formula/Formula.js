8fbdc964dd46292efa1aa30330a411fd4da982bf
panovyk <lromikl@ukr.net>
2021-01-28 23:33:59 +0200

    added listeners deleting method

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,26 +1,27 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 
 export class Formula extends ExcelComponent {
-  static className = 'excel__formula'
+    static className = 'excel__formula'
 
-  constructor($root) {
-    super($root, {
-      name: 'Formula',
-      listeners: ['input', 'click']
-    })
-  }
+    constructor($root) {
+      super($root, {
+        name: 'Formula',
+        listeners: ['input', 'click']
+      })
+    }
 
-  toHTML() {
-    return `
+    toHTML() {
+      return `
             <div class="info">fx</div>
-            <div class="input" contenteditable="true" spellcheck="false"></div>`
-  }
+            <div class="input" contenteditable="true" spellcheck="false"></div>
+            `
+    }
 
-  onInput(event) {
-    console.log('Formula: onInput', event.target.textContent.trim());
-  }
+    onInput(event) {
+      console.log('Formula: onInput', event.target.textContent.trim());
+    }
 
-  onClick() {
-    console.log('mk');
-  }
+    onClick() {
+      console.log('mk');
+    }
 }
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -18,13 +18,17 @@ export class DomListener {
             `Method ${method} is not implemented in ${name} Component`
         )
       }
+      this[method] = this[method].bind(this)
       // 'on' same as addEventListener
       this.$root.on(listener, this[method])
     })
   }
 
   removeDOMListeners() {
-
+    this.listeners.forEach((listener) => {
+      const method = getMethodName(listener)
+      this.$root.off(listener.this[method])
+    })
   }
 }
 
