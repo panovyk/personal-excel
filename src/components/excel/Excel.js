49b6e715422300c2cc33bb0908ce70bcfaa9e932
panovyk <lromikl@ukr.net>
2021-01-28 22:31:28 +0200

    developing framework -added new methods for Formula.js -input method for Formula.js -minor bugs fixed

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -8,20 +8,19 @@ export class Excel {
 
   getRoot() {
     const $root = $.create('div', 'excel')
+
     this.components = this.components.map( (Component) => {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
       $el.html(component.toHTML())
       $root.append($el)
-      // eslint-disable-next-line max-len
-      // $root.insertAdjacentHTML('beforeend', component.toHTML()) - old method without style
+      return component
     })
+
     return $root
   }
 
   render() {
-    const node = document.createElement('h1')
-    node.textContent = 'test321'
     this.$el.append(this.getRoot())
     // check later mb
     this.components.forEach((component) => component.init())
